2331a05e6e1a275c777a917904f337027f647d84
efraitor_ <efraitor.contact@gmail.com>
2023-03-10 16:10:50 +0100

    Change
    Update

---
+++ Assets/Scripts/GridHelper.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Este Script es un Helper(no es necesario relacionarlo con ningún GO)
+public class GridHelper : MonoBehaviour
+{
+    /* Matriz filas y columnaas
+       |  0  |  1  |  2  |  3  |
+    ----------------------------
+     0 | null   x     x    null
+     1 |  x     x    null  null
+     2 | null  null  null  null
+     3 | null  null  null  null
+     */
+
+     //Ancho y alto de la rejilla
+     //Son estáticas para poder instanciarlas sin tener que consultar el objeto, estático es que solo hay uno de ese tipo
+     public static int w = 10, h = 18 + 4;
+     //Creamos el array doble rejilla, de altura y anchura dada
+     public static Transform[,] grid = new Transform[w,h]; //La [,] indica dos dimensiones
+
+     //Método que dado un Vector2 cogerá ese Vector, y redondeará sus coordenadas de X e Y. Tras esto el método nos devuelve el vector redondeado
+     public static Vector2 RoundVector(Vector2 v)
+     {
+          //Devuelve un nuevo Vector2 ya redondeado en X e Y
+          return new Vector2(Mathf.Round(v.x), Mathf.Round(v.y)); //Mathf.Round -> Redondea al número entero más próximo
+     }
+
+     //Método que dada una posición comprobamos si esta pieza está dentro de los bordes del juego. Nos devolverá si es cierto o no
+     public static bool IsInsideBorders(Vector2 pos)
+     {
+          //Si ambas coordenadas son positivas y no se pasan por la derecha
+          if(pos.x >= 0 && pos.y >= 0 && pos.x < w)
+          {
+                //PABLO TROYA TE AMO
+          }
+     }
+}
--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -13,6 +13,25 @@ public class Piece : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Movimiento horizontal de las piezas
+        //Movimiento de la ficha a la izquierda
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            //Llamamos al método de movimiento horizontal y le pasamos la dirección izquierda
+            MovePieceHorizontally(-1);
+        }
+        //Movimiento de la ficha a la derecha
+        else if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            //Llamamos al método de movimiento horizontal y le pasamos la dirección derecha
+            MovePieceHorizontally(1);
+        }
+    }
+
+    //Método para el movimiento horizontal
+    void MovePieceHorizontally(int direction) //con direction, le pasamos un número para saber si el movimiento es a izquierda o a derecha
+    {
+        //Muevo la pieza en la dirección dada
+        transform.position += new Vector3(direction, 0, 0);
     }
 }
